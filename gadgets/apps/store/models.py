b9773c4974aeed6c67920ab54502faeb34032f57
Ada-lave <113159483+Ada-lave@users.noreply.github.com>
2023-05-02 20:05:21 +0300

    cart

--- gadgets/apps/store/models.py
+++ gadgets/apps/store/models.py
@@ -3,10 +3,12 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=120)
     slug = models.SlugField(max_length=255)
+    ordering = models.IntegerField(default=0)
 
 
     class Meta:
         verbose_name_plural = 'Categories'
+        ordering = ('ordering',)
 
     def __str__(self) -> str:
         return self.title
@@ -18,6 +20,11 @@ class Product(models.Model):
     slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    is_featured = models.BooleanField(default=False)
+    data_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-data_added',)
 
     def __str__(self) -> str:
         return self.title
--- gadgets/gadgets/urls.py
+++ gadgets/gadgets/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from apps.core.views import *
 from apps.store.views import *
+from apps.cart.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path("", frontpage, name='frontpage'),
     path('contact/', contact, name='contact'),
+    path('about/', about, name='about'),
+    path('cart/', cart, name='cart'),
 
     path("<slug:category_slug>/", category_detail, name='category_detail'),
     path("<slug:category_slug>/<slug:slug>/", product_detail, name='product_detail'),
